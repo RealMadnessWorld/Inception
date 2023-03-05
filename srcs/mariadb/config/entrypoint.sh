16132c39ab6f47fa9d1f54a20eac1d0b474a4acb
RealMadnessWorld <joaoarsenio@live.com.pt>
2023-03-05 12:45:52 +0000

    fixed mariadb

--- srcs/mariadb/config/entrypoint.sh
+++ srcs/mariadb/config/entrypoint.sh
@@ -2,39 +2,49 @@
 
 # tail -f /dev/null
 
-# sleep infinity
-
-mysql_install_db;
-# mysql_secure_installation;
-# mysqld_safe --skip-grant-tables;
-echo "	[i] Mariadb installed"
-
 service mysql start;
+sleep 5;
 echo "	[i] Mysql started"
 
 # verification if the database exists
 # if it doesn't, creates it
-
 if [ -d /var/lib/mysql/${MARIADB_DATABASE} ]; then
 	echo "	[i] ${MARIADB_DATABASE} already exists"
 else
-	mysql -uroot -p${MARIADB_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
-	mysql -uroot -p${MARIADB_PASSWORD} -e "CREATE USER ${MARIADB_USER}@localhost IDENTIFIED BY '${MARIADB_PASSWORD}';"
-	mysql -uroot -p${MARIADB_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'localhost';"
-	mysql -uroot -p${MARIADB_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}' WITH GRANT OPTION;"
-	mysql -uroot -p${MARIADB_PASSWORD} -e "FLUSH PRIVILEGES;"
-	echo "	[i] Database created"
+	# mysql -uroot -p${MARIADB_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
+	# mysql -uroot -p${MARIADB_PASSWORD} -e "CREATE USER ${MARIADB_USER}@localhost IDENTIFIED BY '${MARIADB_PASSWORD}';"
+	# mysql -uroot -p${MARIADB_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'localhost';"
+	# mysql -uroot -p${MARIADB_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}' WITH GRANT OPTION;"
+	# mysql -uroot -p${MARIADB_PASSWORD} -e "FLUSH PRIVILEGES;"
+	# echo "	[i] Database created"
+
+	# creating database
+	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};"
+	# creating user MARIADB_USER %
+	mariadb -u root -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+	mariadb -u root -e "GRANT ALL ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+	mariadb -u root -e "FLUSH PRIVILEGES;"
+	# creating user MARIADB_USER local
+	mariadb -u root -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+	mariadb -u root -e "GRANT ALL ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+	mariadb -u root -e "FLUSH PRIVILEGES;"
+	echo "${MARIADB_DATABASE} created database"
+	
+	mariadb -u root -e "CREATE TABLE ${MARIADB_DATABASE}.todo_list (item_id INT AUTO_INCREMENT, content VARCHAR(255), PRIMARY KEY(item_id));"
+	mariadb -u root -e "INSERT INTO ${MARIADB_DATABASE}.todo_list (content) VALUES ('My first task, yay');"
+	mariadb -u root -e "INSERT INTO ${MARIADB_DATABASE}.todo_list (content) VALUES ('Second task, gimme more');"
+	mariadb -u root -e "INSERT INTO ${MARIADB_DATABASE}.todo_list (content) VALUES ('Last task i promise');"
+	mariadb -u root -e "INSERT INTO ${MARIADB_DATABASE}.todo_list (content) VALUES ('one more task');"
 
 	# mysql -uroot -p${MARIADB_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
 	mysql -uroot -p${MARIADB_PASSWORD} -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_PASSWORD}');"
-	mysql -uroot -p${MARIADB_PASSWORD} -e "UPDATE mysql.user SET plugin = 'auth_socket' WHERE user = 'root';"
 	mysql -uroot -p${MARIADB_PASSWORD} -e "FLUSH PRIVILEGES;"
 	echo "	[i] Root password updated"
 fi
 
 #SELECT User, Password, Plugin FROM mysql.user;
 
-# service mysql stop;
+mysqladmin -u root -p${MARIADB_PASSWORD} shutdown;
 echo "	[i] Shutdown"
 
 mysqld;
---
+++ srcs/wordpress/config/todo_list.php
@@ -0,0 +1,19 @@
+<?php
+$user = "jarsenio";
+$password = "jarsenio";
+$database = "testdb";
+$host = "mariadb";
+$table = "todo_list";
+​
+try {
+  $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+  echo "<h2>TODO</h2><ol>"; 
+  foreach($db->query("SELECT content FROM $table") as $row) {
+    echo "<li>" . $row['content'] . "</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
